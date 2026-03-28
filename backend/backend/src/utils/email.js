c7a29de5bfec4b7ee6492efe32497cdeb7ceb337
christinecortes <lr.cjcortes@mmdc.mcl.edu.ph>
2026-03-28 23:11:29 +0800

    Use EMAIL_FROM env variable for sender address

--- backend/backend/src/utils/email.js
+++ backend/backend/src/utils/email.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendResetEmail = async (to, resetLink) => {
   await resend.emails.send({
-    from: "Bubbistix Support <onboarding@resend.dev>",
+    from: process.env.EMAIL_FROM,
     to,
     subject: "Reset your Bubbistix password",
     html: `
